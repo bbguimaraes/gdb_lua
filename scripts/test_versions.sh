bb3cfb63a424be7efbd7226cd68949977a406ba4
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-07-17 19:57:25 +0000

    Update supported versions, add test script

---
+++ scripts/test_versions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+
+TESTS='backtrace stack'
+
+main() {
+    local dir
+    for dir; do
+        echo "$dir"
+        build "$dir"
+        exec_tests "$dir"
+    done
+}
+
+build() {
+    local dir=$1
+    make --no-print-directory -C test clean
+    make --no-print-directory -C test \
+        CPPFLAGS="-I $dir/src" LDFLAGS="-L $dir/src"
+}
+
+exec_tests() {
+    local dir=$1 x
+    local name=${1##*/}
+    local ld="$dir/src"
+    for x in $TESTS; do
+        local cmd=(gdb --batch --command "test/$x/$x.gdb" "test/$x/$x")
+        echo LD_LIBRARY_PATH="$ld" "${cmd[@]}"
+        LD_LIBRARY_PATH="$ld" "${cmd[@]}" &> "test/$x/output_$name.txt"
+    done
+}
+
+main "$@"
